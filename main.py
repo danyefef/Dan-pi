a531c501480ccbbc383c53df9d7a2d35a02f345a
danyefef <danyefef@yahoo.com>
2020-04-10 00:20:48 -0400

    Added new branch

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ def main(msg):
     print(msg)
 #This is the beginning
 main("Hello, it's Dansweet")
-<<<<<<< HEAD
 main("I am amazing")
-=======
-#You're amazing
->>>>>>> refs/remotes/origin/master
 main("I love myself so much")
+#No longer need comments here
+def second():
+    print("new function")
